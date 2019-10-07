96c2eaf427570ac9f6009781e2d55d33122b6fe8
Kalib Hackin <nagui08@hotmail.com>
2019-10-07 13:30:09 -0500

    working on the winning conditions

--- script.js
+++ script.js
@@ -11,21 +11,27 @@ const winningArray = [
 
 let boardGame = Array.from(document.querySelectorAll('.s')); 
 
+let identifyId = event => {
+    player1.push(parseInt(event.target.id));
+}
+
+let squares = document.getElementsByClassName('s')
+squares.addEventListener('click', identifyId);
+
 let startGame = document.getElementById('start-game');
 startGame.addEventListener('click', ()=> {
     startGame.style.display = 'none';
 });
 
-let player = 1;
+let player1 = [];
 let counter = 0;
-
 
 boardGame.forEach((square) => {
     square.addEventListener('click', () => {
-        if (counter%2 === 0) {
+        if (counter%2 === 0 && square.innerHTML === '') {
             square.innerHTML = 'X';
             counter++;
-        } else {
+        } else if (counter%2 !== 0 && square.innerHTML === '') {
             square.innerHTML = 'O';
             counter++;
         }
