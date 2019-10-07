d9988e0148f9e0b2f7c8c3ee51fcc89c254b3719
Kalib Hackin <nagui08@hotmail.com>
2019-10-07 15:39:40 -0500

    winning condition done

--- script.js
+++ script.js
@@ -9,31 +9,51 @@ const winningArray = [
     [6, 4, 2]
 ]
 
-let boardGame = Array.from(document.querySelectorAll('.s')); 
+let boardGame = Array.from(document.querySelectorAll('.s'));
 
 let identifyId = event => {
     player1.push(parseInt(event.target.id));
 }
 
-let startGame = document.getElementById('start-game');
-startGame.addEventListener('click', ()=> {
-    startGame.style.display = 'none';
-});
-
 let player1 = [];
 let player2 = [];
 let counter = 0;
 
-boardGame.forEach((square) => {
-    square.addEventListener('click', () => {
-        if (counter%2 === 0 && square.innerHTML === '') {
-            square.innerHTML = 'X';
-            counter++;
-            player1.push(parseInt(event.target.id))
-        } else if (counter%2 !== 0 && square.innerHTML === '') {
-            square.innerHTML = 'O';
-            counter++;
-            player2.push(parseInt(event.target.id))
-        }
+let startGame = document.getElementById('start-game');
+startGame.addEventListener('click', ()=> {
+    startGame.style.display = 'none';
+    boardGame.forEach((square) => {
+        square.classList.add('hover');
+        square.addEventListener('click', () => {
+            if (counter%2 === 0 && square.innerHTML === '') {
+                square.innerHTML = 'X';
+                counter++;
+                player1.push(parseInt(event.target.id));
+                square.classList.remove('hover');
+                if (player1.length >= 3) {
+                    for(let i = 0; i < 8 ; i++){
+                        for (let j = 0; j < player1.length; j++) {
+                            if (winningArray[i].includes(player1[j])) {
+                                board.style.display = 'none';
+                            }
+                        }
+                    }
+                }
+            } else if (counter%2 !== 0 && square.innerHTML === '') {
+                square.innerHTML = 'O';
+                counter++;
+                player2.push(parseInt(event.target.id));
+                square.classList.remove('hover');
+                if (player1.length >= 3) {
+                    for(let i = 0; i < 8 ; i++){
+                        for (let j = 0; j < player1.length; j++) {
+                            if (winningArray[i].includes(player1[j])) {
+                                console.log('ok')
+                            }
+                        }
+                    }
+                }
+            }
+        });
     });
 });
