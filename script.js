a7881457ed815359688d2152fb0619243a219838
bernardogarza <bernardo.g.landa@gmail.com>
2019-10-10 15:31:15 -0500

    added mark and player turn

--- script.js
+++ script.js
@@ -29,67 +29,88 @@ let Game = (()=>{
         player2.squares = [];
         players.style.display = 'none';
     }
+
+    let markSquare = square => {
+        square.target.innerHTML = player.mark;
+        addSqueare(player, square.target.id);
+        disableSquare(square);
+        winner(player);
+        if (gameOver === false) {
+            turn++;
+            playerTurn();
+            playerTurnMessage(player);
+            tieGame(turn);
+        }
+    }
+
+    let playerTurn = () => {
+        if (turn%2 === 1) {
+            player = player1
+        } else {
+            player = player2
+        }
+    }
 })();
 
-let identifyId = event => {
-    player1.push(parseInt(event.target.id));
-}
-
-startGame.addEventListener('click', ()=> {
-    startGame.style.display = 'none';
-    boardGame.forEach((square) => {
-        square.classList.add('hover');
-        square.innerHTML = '';
-        board.forEach(function(e) {e.style.display='inline'});
-        square.addEventListener('click', () => {
-            if (counter%2 === 0 && square.innerHTML === '') {
-                square.innerHTML = 'X';
-                counter++;
-                player1.push(parseInt(event.target.id));
-                square.classList.remove('hover');
-                let number1 = 0;
-                if (player1.length >= 3) {
-                    for(let i = 0; i < 8 ; i++){
-                        for (let j = 0; j < player1.length; j++) {
-                            if (winningArray[i].includes(player1[j])) {
-                                number1++;
-                            }
-                            if (number1 === 3){
-                                board.forEach(function(e) {e.style.display='none'});
-                                alert('Player 1 wins');
-                                resetPlayer();
-                                square.innerHTML = '';
-                                startGame.style.display = 'inline';
-                            }
-                        }
-                        number1 = 0;
-                    }
-                }
-            } else if (counter%2 !== 0 && square.innerHTML === '') {
-                square.innerHTML = 'O';
-                counter++;
-                player2.push(parseInt(event.target.id));
-                square.classList.remove('hover');
-                let number2 = 0;
-                if (player2.length >= 3) {
-                    for(let i = 0; i < 8; i++){
-                        for (let j = 0; j < player2.length; j++) {
-                            if (winningArray[i].includes(player2[j])) {
-                                number2++;
-                            }
-                            if(number2 === 3){
-                                board.forEach(function(e) {e.style.display='none'});
-                                alert('Player 2 wins');
-                                resetPlayer();
-                                startGame.style.display = 'inline';
-                            }
-                        }
-                        number2 = 0;
-                    }
-                }
-            }
-        });
-    });
-});
-
 startGame.addEventListener('click', Game.start);
+
+// let identifyId = event => {
+//     player1.push(parseInt(event.target.id));
+// }
+
+// startGame.addEventListener('click', ()=> {
+//     startGame.style.display = 'none';
+//     boardGame.forEach((square) => {
+//         square.classList.add('hover');
+//         square.innerHTML = '';
+//         board.forEach(function(e) {e.style.display='inline'});
+//         square.addEventListener('click', () => {
+//             if (counter%2 === 0 && square.innerHTML === '') {
+//                 square.innerHTML = 'X';
+//                 counter++;
+//                 player1.push(parseInt(event.target.id));
+//                 square.classList.remove('hover');
+//                 let number1 = 0;
+//                 if (player1.length >= 3) {
+//                     for(let i = 0; i < 8 ; i++){
+//                         for (let j = 0; j < player1.length; j++) {
+//                             if (winningArray[i].includes(player1[j])) {
+//                                 number1++;
+//                             }
+//                             if (number1 === 3){
+//                                 board.forEach(function(e) {e.style.display='none'});
+//                                 alert('Player 1 wins');
+//                                 resetPlayer();
+//                                 square.innerHTML = '';
+//                                 startGame.style.display = 'inline';
+//                             }
+//                         }
+//                         number1 = 0;
+//                     }
+//                 }
+//             } else if (counter%2 !== 0 && square.innerHTML === '') {
+//                 square.innerHTML = 'O';
+//                 counter++;
+//                 player2.push(parseInt(event.target.id));
+//                 square.classList.remove('hover');
+//                 let number2 = 0;
+//                 if (player2.length >= 3) {
+//                     for(let i = 0; i < 8; i++){
+//                         for (let j = 0; j < player2.length; j++) {
+//                             if (winningArray[i].includes(player2[j])) {
+//                                 number2++;
+//                             }
+//                             if(number2 === 3){
+//                                 board.forEach(function(e) {e.style.display='none'});
+//                                 alert('Player 2 wins');
+//                                 resetPlayer();
+//                                 startGame.style.display = 'inline';
+//                             }
+//                         }
+//                         number2 = 0;
+//                     }
+//                 }
+//             }
+//         });
+//     });
+// });
